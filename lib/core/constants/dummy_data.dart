b6646d21a5773ec4253f186c8bd1c57d411c1be8
bima1313 <bimairfy13@gmail.com>
2026-04-22 23:02:07 +0700

    feat: add dummy data constants for development

---
+++ lib/core/constants/dummy_data.dart
@@ -0,0 +1,34 @@
+const dummyData = {
+  "data": [
+    {
+      "title": "Artisan Roasters",
+      "simple_description": "coffee & bakery",
+      "icon_name": "drink",
+      "amount": 12.40,
+    },
+    {
+      "title": "City Cab Transit",
+      "simple_description": "transportation",
+      "icon_name": "transport",
+      "amount": 34.10,
+    },
+    {
+      "title": "2 Shirt",
+      "simple_description": "Cloths",
+      "icon_name": "shopping",
+      "amount": 96.10,
+    },
+    {
+      "title": "2 kg Laundry",
+      "simple_description": "laundry",
+      "icon_name": "other",
+      "amount": 20.00,
+    },
+    {
+      "title": "2 lay's",
+      "simple_description": "snack",
+      "icon_name": "snack",
+      "amount": 3.00,
+    },
+  ],
+};
