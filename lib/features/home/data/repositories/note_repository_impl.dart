6dd57b87f162e72ab1da66d2f268fa6a3453afcf
Bima Hamdhika Irfy <87358505+bima1313@users.noreply.github.com>
2026-04-23 20:05:28 +0700

    Refactor NoteRepositoryImpl to implement NoteRepository

--- lib/features/home/data/repositories/note_repository_impl.dart
+++ lib/features/home/data/repositories/note_repository_impl.dart
@@ -1,10 +1,11 @@
 import 'package:expense_tracker/features/home/data/datasource/note_local_data_source.dart';
 import 'package:expense_tracker/features/home/data/models/note_model.dart';
+import 'package:expense_tracker/features/home/domain/repositories/note_repository.dart';
 
-class NoteRepositoryImpl implements NoteLocalDataSource {
+class NoteRepositoryImpl implements NoteRepository {
   final NoteLocalDataSource localDataSource;
 
-  const NoteRepositoryImpl(this.localDataSource);
+  const NoteRepositoryImpl({required this.localDataSource});
   @override
   Future<Iterable<NoteModel>> getNotes() async {
     final data = await localDataSource.getNotes();
