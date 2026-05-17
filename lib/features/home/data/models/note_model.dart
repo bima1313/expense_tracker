a40112171b42d380cbeeb20d32e32ca079cf7c5a
bima1313 <bimairfy13@gmail.com>
2026-05-17 18:27:32 +0700

    refactor: update to fetch NoteModel data

--- lib/features/home/data/models/note_model.dart
+++ lib/features/home/data/models/note_model.dart
@@ -1,18 +1,16 @@
 import 'package:expense_tracker/features/home/domain/entities/note.dart';
 
 class NoteModel extends Note {
-  NoteModel({
+  const NoteModel({
     required super.title,
-    required super.simpleDescription,
-    required super.iconName,
+    required super.category,
     required super.amount,
   });
 
   factory NoteModel.fromJson(Map<String, dynamic> json) {
     return NoteModel(
       title: json["title"],
-      simpleDescription: json["simple_description"],
-      iconName: json["icon_name"],
+      category: json["category"],
       amount: json["amount"],
     );
   }
