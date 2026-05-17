fe1e93d6991324cfa2af745d11b6d66fc4f2a2db
bima1313 <bimairfy13@gmail.com>
2026-05-17 18:23:24 +0700

    refactor(data): implement Equatable and update database fetching
    - Integrate Equatable into models for optimized state and value comparison.
    - Update repository queries to fetch the latest data structure from the database.

--- lib/features/home/domain/entities/note.dart
+++ lib/features/home/domain/entities/note.dart
@@ -1,13 +1,16 @@
-class Note {
+import 'package:equatable/equatable.dart';
+
+class Note extends Equatable {
   final String title;
-  final String simpleDescription;
-  final String iconName;
+  final String category;
   final double amount;
 
-  Note({
+  const Note({
     required this.title,
-    required this.simpleDescription,
-    required this.iconName,
+    required this.category,
     required this.amount,
   });
+
+  @override
+  List<Object?> get props => [title, category, amount];
 }
